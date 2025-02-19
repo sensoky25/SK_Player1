31ff58313df6272e4fd5fc3af65f97ee7c2ff80c
sensoky25 <154733481+sensoky25@users.noreply.github.com>
2025-02-19 23:37:14 +0700

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,28 @@
+{
+  "kind": "Single Item",
+  "playlist": [
+    {
+      "title": "Dogsledding in Alaska",
+      "mediaid": "jumBvHdL",
+      "link": "https://cdn.jwplayer.com/previews/jumBvHdL",
+      "image": "https://cdn.jwplayer.com/v2/media/jumBvHdL/poster.jpg?width=720",
+      "duration": 500,
+      "pubdate": 1492541625,
+      "description": "Aerial view of professional dogsledding handler with his team of trained husky dogs mountain pass, Alaska, USA, RED EPIC",
+      "tags": "stock video",
+      "sources": [
+        {
+          "file": "https://1a-1791.com/video/s8/2/n/O/i/e/nOieu.caa.mp4",
+          "type": "video/mp4",
+          "height": 1080,
+          "width": 1920,
+          "label": "H.264 1920px",
+          "bitrate": 4137800,
+          "filesize": 11813429,
+          "framerate": 25
+        }
+      ],
+    }
+  ],
+  "feed_instance_id": "23086a3d-b911-45ae-a987-711c25ce14c9"
+}
