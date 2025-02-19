9504c43c918241e0d0cca7cc4476cebcd606dfc7
sensoky25 <154733481+sensoky25@users.noreply.github.com>
2025-02-19 23:15:16 +0700

    Delete 3.js

--- 3.js
+++
@@ -1,6 +0,0 @@
-{
-  "playlist": [{
-    "images": [{"src": "https://cdn.jwplayer.com/v2/media/jumBvHdL/poster.jpg?width=1920",}],
-    "sources": [{"file": "https://cdn.jwplayer.com/videos/jumBvHdL-8yQ1cYbs.mp4"}],
-  }],
-}
