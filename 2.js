abffeb3230189188afc84c4943d05e402fb61c90
sensoky25 <154733481+sensoky25@users.noreply.github.com>
2025-02-19 22:59:24 +0700

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,97 @@
+{
+    "title": "Dogsledding in Alaska",
+    "description": "Aerial view of professional dogsledding handler with his team of trained husky dogs mountain pass, Alaska, USA, RED EPIC",
+    "kind": "Single Item",
+    "playlist": [
+      {
+        "title": "Dogsledding in Alaska",
+        "mediaid": "jumBvHdL",
+        "link": "https://cdn.jwplayer.com/previews/jumBvHdL",
+        "image": "https://cdn.jwplayer.com/v2/media/jumBvHdL/poster.jpg?width=720",
+        "images": [
+          {
+            "src": "https://cdn.jwplayer.com/v2/media/jumBvHdL/poster.jpg?width=1920",
+            "width": 1920,
+            "type": "image/jpeg"
+          }
+        ],
+        "duration": 22,
+        "pubdate": 1492541625,
+        "description": "Aerial view of professional dogsledding handler with his team of trained husky dogs mountain pass, Alaska, USA, RED EPIC",
+        "tags": "stock video",
+        "sources": [
+          {
+            "file": "https://cdn.jwplayer.com/manifests/jumBvHdL.mpd",
+            "type": "application/dash+xml",
+            "mediaTypes": [
+              "video/webm; codecs=\"vp9\""
+            ]
+          },
+          {
+            "file": "https://cdn.jwplayer.com/manifests/jumBvHdL.m3u8",
+            "type": "application/vnd.apple.mpegurl"
+          },
+          {
+            "file": "https://cdn.jwplayer.com/videos/jumBvHdL-Zq6530MP.mp4",
+            "type": "video/mp4",
+            "height": 180,
+            "width": 320,
+            "label": "H.264 320px",
+            "bitrate": 311712,
+            "filesize": 889946,
+            "framerate": 25
+          },
+          {
+            "file": "https://cdn.jwplayer.com/videos/jumBvHdL-TNpruJId.mp4",
+            "type": "video/mp4",
+            "height": 270,
+            "width": 480,
+            "label": "H.264 480px",
+            "bitrate": 561160,
+            "filesize": 1602113,
+            "framerate": 25
+          },
+          {
+            "file": "https://cdn.jwplayer.com/videos/jumBvHdL-cIp6U8lV.mp4",
+            "type": "video/mp4",
+            "height": 406,
+            "width": 720,
+            "label": "H.264 720px",
+            "bitrate": 876720,
+            "filesize": 2503040,
+            "framerate": 25
+          },
+          {
+            "file": "https://cdn.jwplayer.com/videos/jumBvHdL-FctPAkow.mp4",
+            "type": "video/mp4",
+            "height": 720,
+            "width": 1280,
+            "label": "H.264 1280px",
+            "bitrate": 1965840,
+            "filesize": 5612480,
+            "framerate": 25
+          },
+          {
+            "file": "https://cdn.jwplayer.com/videos/jumBvHdL-8yQ1cYbs.mp4",
+            "type": "video/mp4",
+            "height": 1080,
+            "width": 1920,
+            "label": "H.264 1920px",
+            "bitrate": 4137800,
+            "filesize": 11813429,
+            "framerate": 25
+          }
+        ],
+        "tracks": [
+          {
+            "file": "https://cdn.jwplayer.com/strips/jumBvHdL-120.vtt",
+            "kind": "thumbnails"
+          }
+        ],
+        "variations": {
+  
+        }
+      }
+    ],
+    "feed_instance_id": "5d43018b-d49f-4785-9192-4211aaf5be82"
+  }
