daf8c4784f5427a037c32e2618f0d0058539e8fb
sensoky25 <154733481+sensoky25@users.noreply.github.com>
2025-02-19 23:11:04 +0700

    Add files via upload

---
+++ 4.js
@@ -0,0 +1,18 @@
+{
+	playlist: [{
+        image: "{$atts['thumbnail']}",
+        sources: [{file:"{$atts['id']}"},],
+        tracks: [
+            {file:"{$atts['sub_english']}", label: "English", kind: "captions","default": true},
+            {file:"{$atts['sub_khmer']}", label: "Khmer", kind: "captions"},
+            {file:"{$atts['sub_thai']}",label: "Thai",kind: "captions"},
+            {file:"{$atts['sub_korean']}",label: "Korean",kind: "captions"},
+            {file:"{$atts['sub_chinese']}",label: "Chinese",kind: "captions"},
+            {file:"{$atts['sub_vietnam']}",label: "Vietnam",kind: "captions"},
+            {file:"{$atts['sub_french']}",label: "French",kind: "captions"},
+            {file:"{$atts['sub_japanese']}",label: "Japanese",kind: "captions"},
+            {file:"{$atts['sub_indonesian']}",label: "Indonesian",kind: "captions"},
+            {file:"{$atts['sub_malay']}",label: "Maly",kind: "captions"},
+        ]
+        }]
+}
